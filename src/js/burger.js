1fbfc51df8811e08cc4290cf24e58750711ee580
Vera Krumpan <filimonovavera@gmail.com>
2025-08-18 11:47:11 +0300

    feat: add adaptive at favorites and about

--- src/js/burger.js
+++ src/js/burger.js
@@ -3,7 +3,6 @@ const burgerBtn = document.querySelector('.burger-btn');
 const header = document.querySelector('.header');
 const navList = document.querySelector('.nav__list');
 const headerBox = document.querySelector('.header__box');
-console.log(headerBox);
 
 burgerBtn.addEventListener('click', () => {
   body.classList.add('scroll-hidden');
